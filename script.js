d7f3f32dcd8d4bbaa9ec962ba9eb815f81939874
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-21 08:01:41 +0530

    Modifying the file

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // creating a javascript Array
 
-let text ["Initializing Hacking..."
-"Reading your Files..."
-"Password files Detected..."
-"Sending all passwords and personal files to server..."
-"Cleaning up..."]
+let text = ["Initializing Hacking...",
+    "Reading your Files...",
+    "Password files Detected...",
+    "Sending all passwords and personal files to server...",
+    "Cleaning up..."]
+
+// iterating the loop using for of loop 
