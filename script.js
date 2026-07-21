faa2c88376eb01dcf9e3eb254baedea391a047c2
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-21 12:39:44 +0530

    Modifying the file

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ async function main() {
 
     setInterval(() => {
         let last = document.body.lastElementChild;
+        // to get exactly three dots we add this if else statement 
+        if(last.innerHTML.endsWith("...")){
+            last.
+        }
+
+
         last.innerHTML = last.innerHTML + "."
 
     }, 700);
